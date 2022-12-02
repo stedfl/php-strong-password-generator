5d4726f7a08e9eedfd10da1b8dd78ae098405e80
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-02 18:24:39 +0100

    add checkbox choices
    Add checkbox for choising what type of characters (numbers, letters,
    special characters) to include in generated password

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function getRandomPsw($length, $array) {
   for ($i=0; $i < $length; $i++) {
     $randomIndex = rand(0, (count($array) - 1));
     $password .= $array[$randomIndex];
+    header('Location: ./success.php');
   }
   return $password;
 }
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require_once  __DIR__ . '/functions.php';
 
 $isCorrectLength = false;
+
+
 if(isset($_GET['psw_length'])) {
   if($_GET['psw_length'] > 7 && $_GET['psw_length'] < 33 ) {
     $isCorrectLength = true;
@@ -11,18 +13,47 @@ if(isset($_GET['psw_length'])) {
   }
 }
 
+
+
 $letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 $numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 $special_characters = ['!', '?', '&', '%', '$', '<', '>', '^', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '@','#', '_', '='];
 
-$letNumbChar = array_merge($letters, $numbers, $special_characters);
-
-if($isCorrectLength) {
-  session_start();
-  $_SESSION['password'] = getRandomPsw($_GET['psw_length'], $letNumbChar);
-  header('Location: ./success.php');
+session_start();
+$mergedArray = [];
+if($isCorrectLength && isset($_GET['characters'])) {
+  if(count($_GET['characters']) === 3 ) {
+    $mergedArray = array_merge($letters, $numbers, $special_characters);
+  } else if (count($_GET['characters']) === 2 ) {
+    if(in_array('0' , $_GET['characters']) && in_array('1' , $_GET['characters']) ) {
+      $mergedArray = array_merge($letters, $numbers);
+    } else if (in_array('0' , $_GET['characters']) && in_array('2' , $_GET['characters'])) {
+      $mergedArray = array_merge($letters, $special_characters);
+    } else {
+      $mergedArray = array_merge($numbers, $special_characters);
+    }
+  } else {
+    if(in_array('1' , $_GET['characters'])) {
+      if ($_GET['psw_length'] > count($numbers)) {
+        $_GET['psw_length'] = count($numbers);
+      }
+      $mergedArray = $numbers;
+    } else if (in_array('2' , $_GET['characters'])) {
+      if ($_GET['psw_length'] > count($special_characters)) {
+        $_GET['psw_length'] = count($special_characters);
+      }
+      $mergedArray = $special_characters;
+    } else {
+      $mergedArray = $letters;
+    }
+  }
+  $_SESSION['password'] = getRandomPsw($_GET['psw_length'], $mergedArray );
+} else if (($isCorrectLength && !isset($_GET['characters']))) {
+  $mergedArray = array_merge($letters, $numbers, $special_characters);
+  $_SESSION['password'] = getRandomPsw($_GET['psw_length'], $mergedArray );
 }
+
 ?>
 
 
@@ -52,12 +83,39 @@ if($isCorrectLength) {
       <?php endif; ?>
       <div class="settings p-3 mt-3">
         <form action="" method="GET">
-          <div class="d-flex align-items-center mb-3">
+          <div class="d-flex align-items-center">
             <label for="exampleFormControlInput1" class="form-label me-2 w-50">Lunghezza password:</label>
             <input type="number" name="psw_length" class="form-control w-25 " id="exampleFormControlInput1" placeholder="numero">
           </div>
-          <button class="btn btn-primary" type="submit">Invia</button>
-          <button class="btn btn-secondary" type="reset">Annulla</button>
+
+          <div class="d-flex align-items-center my-5">
+            <label for="exampleFormControlInput1" class="form-label me-2 w-50">Scegli se includere:</label>
+            <div class="check">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="0" name="characters[]" id="flexCheckIndeterminateDisabled">
+                <label class="form-check-label" for="flexCheckIndeterminateDisabled">
+                  Lettere
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="1" name="characters[]" id="flexCheckIndeterminateDisabled">
+                <label class="form-check-label" for="flexCheckIndeterminateDisabled">
+                  Numeri
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="2" name="characters[]" id="flexCheckIndeterminateDisabled">
+                <label class="form-check-label" for="flexCheckIndeterminateDisabled">
+                  Caratteri speciali
+                </label>
+              </div>
+            </div>
+          </div>
+          
+          <div class="buttons">
+            <button class="btn btn-primary" type="submit">Invia</button>
+            <button class="btn btn-secondary" type="reset">Annulla</button>
+          </div>
         </form>
       </div>
     </div>
