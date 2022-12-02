cc3c780aff1f001c2e57b1891146447b5bcc188a
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-02 15:37:04 +0100

    add 3 arrays as datas with letters, numbers and special charactes
    Create another array where merged others

--- index.php
+++ index.php
@@ -11,6 +11,13 @@ if(isset($_GET['psw_length'])) {
   var_dump($isCorrectLength);
 }
 
+$letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+$numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+$special_characters = ['!', '?', '&', '%', '$', '<', '>', '^', '+', '-', '*', '/', '(', ')', '[', ']', '{', '}', '@','#', '_', '='];
+
+$letNumbChar = array_merge($letters, $numbers, $special_characters);
+
   
 
 ?>
